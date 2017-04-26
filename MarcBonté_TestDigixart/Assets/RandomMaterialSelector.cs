f619d353bc5779e9936a9ea9cc4a6b8827ca6675
Marc <m.bonte@rubika-edu.com>
2017-04-27 00:35:53 +0200

    Added a few meshes

---
+++ MarcBonté_TestDigixart/Assets/RandomMaterialSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterialSelector : MonoBehaviour
+{
+    [SerializeField]
+    private bool m_GiveNewMaterial;
+    [SerializeField]
+    private Material[] m_Materials;
+
+    private void OnValidate()
+    {
+        if (m_GiveNewMaterial)
+        {
+            m_GiveNewMaterial = false;
+
+            int randomMaterial = Random.Range(0, m_Materials.Length - 1);
+            GetComponent<MeshRenderer>().material = m_Materials[randomMaterial];
+        }
+    }
+}
